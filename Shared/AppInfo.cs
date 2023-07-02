289c4e6540cb2249403e1b61e72e964c63fc9f33
Simon Dudley <simon_dudley@msn.com>
2023-07-02 21:14:23 +1000

    added About page; some refactoring

---
+++ Shared/AppInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class AppInfo
+    {
+        private readonly static AppInfo _global = new AppInfo();
+        public static AppInfo Global => _global;
+
+        public readonly string Name;
+        public readonly Version Version;
+        private AppInfo()
+        {
+            Name = "MyFamily";
+            Version = new Version(1, 0, 0);
+        }
+
+    }
+}
