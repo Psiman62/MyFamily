58083362f4022061445936de357b58070ff3980f
Simon Dudley <simon_dudley@msn.com>
2024-02-29 19:15:13 +1000

    updated dependencies

--- Shared/AppInfo.cs
+++ Shared/AppInfo.cs
@@ -7,12 +7,14 @@
 
         public readonly string Name;
         public readonly string AppVersion;
+        public readonly string CommitDate;
         public readonly string CG4Version;
 
         private AppInfo()
         {
             Name = "MyFamily";
             AppVersion = ThisAssembly.AssemblyFileVersion;
+            CommitDate = ThisAssembly.GitCommitDate.ToString("R");
             CG4Version = MetaFac.CG4.Runtime.BuildInfo.Instance.AssemblyFileVersion;
         }
 
